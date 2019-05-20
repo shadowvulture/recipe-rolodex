3502afcc842d04783c549decc7bb528f965be049
Gabriella De Francesco <gabriella.defrancesco92@gmail.com>
2019-05-20 14:15:54 -0400

    added names to planning images, updated navbar create and home routes to match back end

--- src/Navbar/Navbar.js
+++ src/Navbar/Navbar.js
@@ -6,10 +6,10 @@ const Navbar = () => {
     return (
         <div>
             <nav className="navbar">
-                <NavLink to= "/">
+                <NavLink to= "/api/">
                     <h1>Recipe Rolodex</h1>
                 </NavLink>
-                <NavLink to= "/create">
+                <NavLink to= "/recipes/create">
                     <h2>Create New Recipe</h2>
                 </NavLink>
             </nav>  
