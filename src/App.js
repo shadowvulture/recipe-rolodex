3836c19642f966b1cc85d1c1f80367437e3dce69
Derek Christensen <covfefe24@protonmail.com>
2019-05-20 16:03:48 -0400

    Built About page with bg image, description, and show all recipes

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import { Route, Link, NavLink, Switch, Redirect } from 'react-router-dom';
 import Navbar from './Navbar/Navbar'
+import About from './About/About'
 
 function App() {
   return (
@@ -9,6 +10,8 @@ function App() {
       <header className="App-header">
           <Navbar/>
       </header>
+      <About />
+     
     </div>
   );
 }
