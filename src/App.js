e644c281f06b47a6a211a05bbef03cb071387ae6
Gabriella De Francesco <gabriella.defrancesco92@gmail.com>
2019-05-20 14:40:55 -0400

    added routes for individual recipe and for home

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import { Route, Link, NavLink, Switch, Redirect } from 'react-router-dom';
 import Navbar from './Navbar/Navbar'
+import Show from './Show/Show'
 
 function App() {
   return (
@@ -9,6 +10,12 @@ function App() {
       <header className="App-header">
           <Navbar/>
       </header>
+      <main>
+        <Switch>
+          <Route exact path= "/api/" component={About} />
+          <Route exact path="/api/recipes/:recipeID" render= {(routerProps) => <Show {...routerProps}/>}/>
+        </Switch>
+      </main>
     </div>
   );
 }
