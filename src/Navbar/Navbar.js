4c0ebe3d5b604c6435b4217a36bfb22c9e1b5599
Gabriella De Francesco <gabriella.defrancesco92@gmail.com>
2019-05-20 13:54:04 -0400

    added css to navbar component

--- src/Navbar/Navbar.js
+++ src/Navbar/Navbar.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import { Route, Link, NavLink, Switch, Redirect } from 'react-router-dom';
+import './Navbar.css';
 
 const Navbar = () => {
     return (
         <div>
-            <nav>
+            <nav className="navbar">
                 <NavLink to= "/">
                     <h1>Recipe Rolodex</h1>
                 </NavLink>
                 <NavLink to= "/create">
-                    <i className="fas fa-plus"></i>
                     <h2>Create New Recipe</h2>
                 </NavLink>
             </nav>  
