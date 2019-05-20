0f6da2dba38bc1d2e0f203afd1b475e161c85c92
Gabriella De Francesco <gabriella.defrancesco92@gmail.com>
2019-05-20 16:19:12 -0400

    updated planning folder

--- src/About/About.js
+++ src/About/About.js
@@ -6,7 +6,7 @@ const About = () => {
     return (
         <div>
             <div className = "background">
-            <img src="" className="mainLogo"alt="Recipe Rolodex Logo"></img>
+            <img src="../rustic-vegan.jpg" className="mainLogo"alt="Recipe Rolodex Logo"></img>
             <div className="about">
            <p className = "desc">
                Recipe Rolodex is a place where users store their favorite recipes in a digital space. Having all your recipes in one place makes it very easy to search through your recipes. Convert Grandma’s time honored-recipes into your own digital recipe repository where they can be cherished forever.
