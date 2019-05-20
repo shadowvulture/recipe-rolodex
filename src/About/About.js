8b5622b9fcbe0e68f0ae7293a72e37654b71daab
Derek Christensen <covfefe24@protonmail.com>
2019-05-20 17:08:19 -0400

    Adjusted css styles for About page

--- src/About/About.js
+++ src/About/About.js
@@ -6,16 +6,16 @@ const About = () => {
     return (
         <div>
             <div className = "background">
-            <img src="" className="mainLogo"alt="Recipe Rolodex Logo"></img>
+            <img src="https://via.placeholder.com/650x150" className="mainLogo"alt="Recipe Rolodex Logo"></img>
             <div className="about">
            <p className = "desc">
-               Recipe Rolodex is a place where users store their favorite recipes in a digital space. Having all your recipes in one place makes it very easy to search through your recipes. Convert Grandma’s time honored-recipes into your own digital recipe repository where they can be cherished forever.
+               Recipe Rolodex is a place where users store their favorite recipes in a digital space. Having all your recipes in one place makes it very easy to search through your recipes. Convert Grandma’s time-honored recipes into your own digital recipe repository where they can be cherished forever.
            </p> 
+           </div>
            <div className ="showAll">
                 <p className="showAllText">
                 Show All Recipes
                 </p>
-           </div>
            </div>
            </div>
         </div>
