9c1e16cac33ed94ee8da06f55748f7858e0a1941
Gabriella De Francesco <gabriella.defrancesco92@gmail.com>
2019-05-22 14:54:16 -0400

    fixed nav bar on the about page

--- src/Navbar/Navbar.js
+++ src/Navbar/Navbar.js
@@ -4,16 +4,16 @@ import './Navbar.css';
 
 const Navbar = () => {
     return (
-        <div>
+        <div className="navbar-container">
             <nav className="navbar">
                 <NavLink to= "/">
                     <h1><i class="material-icons md-24">tab</i>Recipe Rolodex</h1>
                 </NavLink>
                 <NavLink to ="/api/recipes">
-                    <h2>Show All Recipes</h2>
+                    <h2 className="show-all-recipes">Show All Recipes</h2>
                 </NavLink>
                 <NavLink to= "/recipes/create">
-                    <h2>Create New Recipe</h2>
+                    <h2 className="create-new-recipe">Create New Recipe</h2>
                 </NavLink>
             </nav>  
         </div>
