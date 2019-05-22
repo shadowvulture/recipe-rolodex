95d16ca2da9439322e34a2847eefd5b424277211
Gabriella De Francesco <gabriella.defrancesco92@gmail.com>
2019-05-22 13:53:50 -0400

    styled about page and moved show all recipe button

--- src/Navbar/Navbar.js
+++ src/Navbar/Navbar.js
@@ -9,6 +9,9 @@ const Navbar = () => {
                 <NavLink to= "/">
                     <h1><i class="material-icons md-24">tab</i>Recipe Rolodex</h1>
                 </NavLink>
+                <NavLink to ="/api/recipes">
+                    <h2>Show All Recipes</h2>
+                </NavLink>
                 <NavLink to= "/recipes/create">
                     <h2>Create New Recipe</h2>
                 </NavLink>
