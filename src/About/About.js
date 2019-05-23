48cd7b45c9b5c4359431003e3b22800a40615411
Gabriella De Francesco <gabriella.defrancesco92@gmail.com>
2019-05-23 09:08:43 -0400

    styled trash icons, added description on about page

--- src/About/About.js
+++ src/About/About.js
@@ -9,6 +9,10 @@ const About = () => {
             <div className="largeLogo">
                 <h1>Recipe Rolodex</h1>
             </div>
+            <div className="about-text">
+               <p>Recipe Rolodex is a place where users store their favorite recipes in a digital space. Having all your recipes in one place makes it very easy to search through your recipes. Convert Grandma’s time honored-recipes into your own digital recipe repository where they can be cherished forever.
+                </p> 
+            </div>
         </div>
     );
 };
