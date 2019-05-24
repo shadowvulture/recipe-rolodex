b3aa1ff6bc9c54c5852a8926186a85bc263371a8
Nathan Rotach <Nathan.Rotach@gmail.com>
2019-05-24 10:58:38 -0400

    blurred password on signupform

--- src/AuthenticateUser/SignUpForm.js
+++ src/AuthenticateUser/SignUpForm.js
@@ -20,7 +20,7 @@ class SignUpForm extends Component {
 
                     <div>
                         <label htmlFor="password">Password</label>
-                        <input type="text" name="password" onChange={this.props.handleInput} />
+                        <input type="password" name="password" onChange={this.props.handleInput} />
                     </div>
                     <input value="Submit" type="submit" onClick={this.props.handleSignUp} />
                 </form>
