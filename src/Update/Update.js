b449f85008cf907ec8b0622f492e0eadde35de08
Derek Christensen <covfefe24@protonmail.com>
2019-05-27 18:47:59 -0400

    Added handleEvent and updateRecipe functions to update page.  Axios put req still not quite working

--- src/Update/Update.js
+++ src/Update/Update.js
@@ -5,7 +5,15 @@ import './Update.css'
 class Update extends Component {
     constructor(props) {
         super (props)
-        this.state = {        }
+        this.state = {
+            Title: '',
+            Thumbnail: '',
+            Cooktime: '',
+            Instructions: ''
+
+                }
+                this.handleChange = this.handleChange.bind(this);
+                this.updateRecipe = this.updateRecipe.bind(this);
     }
 
     componentDidMount = () => {
@@ -28,17 +36,55 @@ class Update extends Component {
         })
     
       }
+      
+      handleChange(event) {
+        event.preventDefault();
+        console.log(event.target.name)
+  
+        this.setState({ [event.target.name] : event.target.value});
+        console.log(this.state);
+        
+      }
+      
+      updateRecipe = () => {
+        const URL = 'https://reactreciperolodex.herokuapp.com/api/recipe/update/' + this.props.match.params.id
+          
+          axios.put(URL, {
+            Title : this.state.Title,
+            Thumbnail : this.state.Thumbnail,
+            Cooktime: this.state.Cooktime,
+            Instructions: this.state.Instructions,
+            headers: {"Content-Type": "application/json"}
+          }
+              
+              
+          )
+          .then((res) => {
+              console.log(res)
+              console.log(URL)
+          })
+      }
 
     render() {
         let recipeDiv
         if (this.state && this.state.recipe) {
-            recipeDiv = <div>
-                Testing {this.state.recipe.Title}
+            recipeDiv = <div className="recipeDiv">
+               <form onSubmit={this.handleSubmit}>
+                Recipe Name: <br />
+                <input name="Title" onChange={this.handleChange} placeholder={this.state.recipe.Title}></input> <br />
+                Recipe Image URL: <br />
+                <input name="Thumbnail" onChange={this.handleChange} placeholder={this.state.recipe.Thumbnail}></input> <br />
+                Estimated Cooking Time: <br />
+                <input name="Cooktime" onChange={this.handleChange} placeholder={this.state.recipe.Cooktime}></input> <br />
+                Instructions: <br />
+                <input name="Instructions" onChange={this.handleChange} placeholder={this.state.recipe.Instructions}></input> <br />
+                <button type="submit" className="updateBtn">Update Recipes</button>
+                </form>
             </div>
         }
         return (
             <div className="ingredientsInput">
-                here
+                
                 {recipeDiv}
             </div>
         );
