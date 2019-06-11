7a1ba9e5e4bc605d81bb5062611492ac9a25b7d5
Derek Christensen <covfefe24@protonmail.com>
2019-06-11 12:50:39 -0400

    Got scrollview working on homepage

--- react-restart/blog-app/App.js
+++ react-restart/blog-app/App.js
@@ -3,6 +3,7 @@ import { Button, View, Text, Image, StyleSheet, TouchableOpacity } from 'react-n
 import { createAppContainer, createStackNavigator } from 'react-navigation'; // Version can be specified in package.json
 import axios from 'axios'
 import { Dimensions } from "react-native";
+import { ScrollView } from 'react-native-gesture-handler';
 
 class HomeScreen extends React.Component {
   constructor () {
@@ -10,22 +11,23 @@ class HomeScreen extends React.Component {
     this.state = {
         article: []
     }
+    console.log(this.props)
 }
 componentDidMount () {
     axios.get('https://p4back.herokuapp.com/api/article')
       .then((res) => {
-        console.log(res)
+        // console.log(res)
         this.setState({
           article: res.data
         }, () => {
-          console.log('after', this.state)
+          // console.log('after', this.state)
         })
       })
       .catch((err) => {
         console.log(err)
 
       })
-      console.log('before', this.state)
+      // console.log('before', this.state)
   }
 
   render() {
@@ -34,16 +36,17 @@ componentDidMount () {
       return (
   
         <View>
+          <ScrollView>
                   
               {this.state.article.map((post => {
       // const {navigate} = this.props.navigation;
       return (
                 
-                <View 
+                <View
                 
                 style={styles.articleView}>
                   <TouchableOpacity 
-                  onPress={() => this.props.navigation.navigate('Details')}
+                  onPress={() => this.props.navigation.navigate('Details', {article: post})}
                   style={styles.articleView}>
               <Image style ={styles.image}
                 source = {{uri: post.urlToImage}} />
@@ -54,11 +57,11 @@ componentDidMount () {
               <Text style={styles.title}>{post.title}</Text>
               </TouchableOpacity>
               </View>
-              
+             
               
             )
             }))}   
-  
+                </ScrollView>
               </View>
       );
     } else {
@@ -82,24 +85,55 @@ componentDidMount () {
 }
 
 class DetailsScreen extends React.Component {
+  constructor (props) {
+    super (props)
+    // this.deleteTeam = this.deleteTeam.bind(this)
+    
+}
+  
   render() {
-    return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Details Screen</Text>
+    var str = this.props.navigation.state.params.article.content
+    var blogData = str.replace(/\\/g, "");
+    console.log('testy', this.props.navigation.state.params.article.title)
+    // if (this.props.navigation.state.params.article.length > 0 ) {
+      console.log('eureka', this.props.navigation.state.params.article.title)
+      return(
+        <View>
+          <Image style ={styles.image}
+              source = {{uri: this.props.navigation.state.params.article.urlToImage}} />
+          <Text 
+              style={styles.blog}>
+              {this.props.navigation.state.params.article.title}</Text>
+          <Image style = {styles.authorPic}
+              source={require('./assets/derek_circle.jpg')} />
+          <Text style={styles.blog}>
+              By: {this.props.navigation.state.params.article.author}</Text>
+          <Text 
+              style={styles.blogContent}>
+              {blogData}</Text>        
         <Button
           title="Go to Details... again"
           onPress={() => this.props.navigation.push('Details')}
         />
         <Button
-          title="Go to Home"
-          onPress={() => this.props.navigation.navigate('Home')}
+            title="Go to Home"
+            onPress={() => this.props.navigation.navigate('Home')}
         />
         <Button
-          title="Go back"
-          onPress={() => this.props.navigation.goBack()}
+            title="Go back"
+            onPress={() => this.props.navigation.goBack()}
         />
-      </View>
-    );
+        </View>
+      )
+    // } else {
+    //   console.log('crap')
+    // }
+    // return (
+      
+    //   <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        
+    //   </View>
+    // );
   }
 }
 
@@ -135,6 +169,20 @@ const styles = StyleSheet.create({
       marginTop: 8,
       
   },
+  blog: {
+    fontSize: 16,
+    textAlign: 'center',
+    alignItems: 'center',
+  },
+  blogContent: {
+    fontSize: 16,
+    textAlign: 'center',
+    alignItems: 'center',
+    paddingTop: 15,
+    marginLeft: 10,
+    marginRight: 10
+    
+  },
   title: {
     fontSize: 20,
     textAlign: 'center',
@@ -150,5 +198,11 @@ const styles = StyleSheet.create({
   borderColor: 'black',
   // backgroundColor: 'white',
   
+  },
+  authorPic : {
+    width: 80,
+    height: 80,
+    marginTop: 10
   }
 })
+console.log('details', this.props)
